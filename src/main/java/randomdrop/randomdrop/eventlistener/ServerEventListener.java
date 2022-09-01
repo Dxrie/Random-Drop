98532b19781cce319039f12f7dc542712d969ab2
Dxrie <colinedric04@gmail.com>
2022-09-01 13:10:04 +0700

    Very cool plugin

--- src/main/java/randomdrop/randomdrop/eventlistener/ServerEventListener.java
+++ src/main/java/randomdrop/randomdrop/eventlistener/ServerEventListener.java
@@ -3,7 +3,11 @@ package randomdrop.randomdrop.eventlistener;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
@@ -28,11 +32,15 @@ public class ServerEventListener implements Listener {
     public void onBlockBreak(BlockBreakEvent e) {
         World world = e.getPlayer().getWorld();
         int length = Randomdrop.itemList.size();
-        ItemStack randomItem = new ItemStack(Randomdrop.itemList.get(random.nextInt(length)), random.nextInt(plugin.getConfig().getInt("randomDropRange")));
+        int range = random.nextInt(plugin.getConfig().getInt("randomDropRange")) + 10;
+        ItemStack randomItem = new ItemStack(Randomdrop.itemList.get(random.nextInt(length)), range);
 
         e.setDropItems(false);
         try {
             world.dropItemNaturally(e.getBlock().getLocation(), randomItem);
-        } catch (IllegalArgumentException exception) {}
+        } catch (IllegalArgumentException exception) {
+            ((TNTPrimed) world.spawnEntity(e.getPlayer().getLocation(), EntityType.PRIMED_TNT)).setFuseTicks(1 * 20);
+            Bukkit.getLogger().info("trying to spawn tnt");
+        }
     }
 }
